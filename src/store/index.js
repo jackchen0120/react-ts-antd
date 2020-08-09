89e6c122c07b544b3e034706c0f0cdb266e6c5c1
jackchen0120 <278910933@qq.com>
2020-08-10 02:09:07 +0800

    添加注释及MIT

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,10 @@
+// 创建一个store管理仓库，从redux库中引入一个createStore函数和applyMiddleware方法
 import { createStore, applyMiddleware } from 'redux';
+// 引入thunk中间件，实现异步action、打印日志、错误报告等功能，放入applyMiddleware方法之中
 import thunk from 'redux-thunk';
 import reducer from './reducers';
+
+// 引入redux-persist库，全局数据持久化存储
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
@@ -13,6 +17,7 @@ const persistConfig = {
 
 const myPersistReducer = persistReducer(persistConfig, reducer)
 
+// 引入createStore后，store并没有创建，需要调用createStore()后才有store
 const store = createStore(
     myPersistReducer,
     applyMiddleware(thunk),
