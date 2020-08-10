10d567057dbeb9e50b64cde9aaa3d67bd0636ab9
jackchen0120 <278910933@qq.com>
2020-08-10 18:16:18 +0800

    代码优化

--- config-overrides.js
+++ config-overrides.js
@@ -1,66 +1,68 @@
 const {
-    override,
-    fixBabelImports,
-    addLessLoader,
-    addWebpackPlugin, 
-    addWebpackAlias,
-    addDecoratorsLegacy,
-    overrideDevServer
-  } = require('customize-cra');
+  override,
+  fixBabelImports,
+  addLessLoader,
+  addWebpackPlugin, 
+  addWebpackAlias,
+  addDecoratorsLegacy,
+  overrideDevServer
+} = require('customize-cra');
 
-  const path = require('path');
-  const webpack = require('webpack');
-  const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-  const CompressionWebpackPlugin = require('compression-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const CompressionWebpackPlugin = require('compression-webpack-plugin');
 
-  const isProduction = process.env.NODE_ENV === 'production';
+const isProduction = process.env.NODE_ENV === 'production';
 
-  function resolve (pathUrl) {
-    return path.join(__dirname, pathUrl);
+function resolve (pathUrl) {
+  return path.join(__dirname, pathUrl);
+}
+
+const addCustomize = () => (config) => {
+  // 配置打包后的文件位置
+  // config.output.path = resolve('dist');
+  if (config.output.publicPath) {
+    config.output.publicPath = '/';
+      // isProduction ? '/react-ts-antd-admin/' : '/';
   }
 
-  const addCustomize = () => (config) => {
-    // 配置打包后的文件位置
-    // config.output.path = resolve('dist');
-    if (config.output.publicPath) {
-      config.output.publicPath = '/';
-        // isProduction ? '/react-ts-antd-admin/' : '/';
-    }
+  if (config.resolve) {
+    config.resolve.extensions = ['.js', '.tsx', '.less', '.css'];
+  }
 
-    if (config.resolve) {
-      config.resolve.extensions = ['.js', '.tsx', '.less', '.css'];
-    }
+  // 添加js、css打包gzip配置
+  config.plugins.push(
+    new CompressionWebpackPlugin({
+      algorithm: 'gzip', // 算法
+      test: /\.js$|\.css$/,  // 压缩 js 与 css
+      threshold: 1024, // 只处理比这个值大的资源，按字节计算
+      // minRatio: 0.8 // 只有压缩率比这个值小的资源才会被处理
+    }),
+  )
 
-    // 添加js、css打包gzip配置
-    config.plugins.push(
-      new CompressionWebpackPlugin({
-        test: /\.js$|\.css$/,
-        threshold: 1024,
-      }),
-    )
+  return config;
+};
 
-    return config;
-  };
-
-  // 设置代理
-  const devServerConfig = () => config => {
-    return {
-      ...config,
-      proxy: {
-        '/api': {
-          target: 'http://106.55.168.13:9000',
-          changeOrigin: true,
-          secure: false
-        }
+// 设置代理
+const devServerConfig = () => config => {
+  return {
+    ...config,
+    proxy: {
+      '/api': {
+        target: 'http://106.55.168.13:9000',
+        changeOrigin: true,
+        secure: false
       }
-    };
+    }
   };
+};
 
-  // 关掉 sourceMap
-  process.env.GENERATE_SOURCEMAP = isProduction ? 'false' : 'true';
+// 关掉 sourceMap
+process.env.GENERATE_SOURCEMAP = isProduction ? 'false' : 'true';
 
-  module.exports = {
-    webpack: override(
+module.exports = {
+  webpack: override(
     // 判断环境，只有在生产环境的时候才去使用这个插件
     isProduction && addWebpackPlugin(new UglifyJsPlugin({
       uglifyOptions: {
@@ -69,7 +71,10 @@ const {
           drop_console: true
         }
       }
-    })),  
+    })),
+    
+    // 启用装饰器语法
+    addDecoratorsLegacy(),
       
     // 配置antd按需引入
     fixBabelImports('import', {
@@ -88,11 +93,8 @@ const {
     addWebpackAlias({
       '@': resolve('src'),
     }),
-    
-    // 支持装饰器
-    addDecoratorsLegacy(),
 
-    // 压缩JS等
+    // 压缩JS、CSS等
     addCustomize()
   ),
 
